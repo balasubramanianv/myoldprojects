c1252fa59b1dee5a0a546add45dd3dd7760c6627
balasubramanianv <balasubramanianv@outlook.com>
2013-09-24 00:20:31 +0530

    Create new_rows.php

---
+++ new_rows.php
@@ -0,0 +1,21 @@
+<?php
+
+	include 'connection.php';
+	
+	$sel = mysql_query("SELECT MAX(table) FROM cust_table");
+	$row_list = array();
+	
+	if(!$sel)
+	{
+		echo("error ".mysql_error());
+	}
+
+		while($rw = mysql_fetch_assoc($sel))
+		{
+			//$row_list[] = array('row'=>$row['MAX(_id)']);
+			$row_list = array('post'=>$rw);
+		}
+		echo  str_ireplace('\\','',str_ireplace('\r\n','',json_encode(array('rows'=>$rows_list))));
+	
+	
+?>
